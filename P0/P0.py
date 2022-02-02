913528180d716b12036981321c46665fbac59a01
IsmaelHC <97998567+IsmaelHC@users.noreply.github.com>
2022-02-02 08:54:50 -0600

    Add time measure for a vector of 2**n

--- P0/P0.py
+++ P0/P0.py
@@ -37,3 +37,19 @@ datos = [1.6, 4.6, 2.6, 3.6, 5.6, 6.6, 3.5, 2.2, 4.4, 5.2, 5.4, 7.6, 5.8, 4.4, 6
 plt.hist(datos)
 plt.show()
 plt.close()
+
+from random import randint
+desde = 1
+hasta = 1000
+menor = 15
+mayor = 21
+from time import time # traer libreria
+for k in range(menor, mayor + 1):
+    n = 2 ** k
+    lista = [ randint(desde, hasta) for i in range(n) ]
+    antes = time() # ver la hora
+    lista.sort()
+    despues = time() # volver ver la hora
+    diferencia = despues - antes # segundos
+    print('ordenar', n, 'elementos toma', diferencia, 'segundos')
+
