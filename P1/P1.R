963b48c88d5ea4760dab0d8b06bf13e879f5e019
IsmaelHC <97998567+IsmaelHC@users.noreply.github.com>
2022-02-11 21:49:17 -0600

    Create P1.R
    P1 genera caminatas aleatorias (Moviemiento Browniano) para 1,2,3,4 y 5 dimensiones las caminatas se realizan 30 veces para cada dimensión y para 100, 1000 y 10000 pasos.

---
+++ P1/P1.R
@@ -0,0 +1,50 @@
+p1=c(0,0)
+cor=30
+dur <- c(100,1000,10000)
+num_ejes<-5
+eje<-0
+#DISTANCIA=rep(0,cor*num_ejes*length(dur))
+DISTANCIA=numeric()
+mayor<-array(rep(0,cor*num_ejes*length(dur)),dim=c(cor,num_ejes,length(dur)))
+pos<- array(rep(0, cor*num_ejes*num_ejes), dim=c(cor,num_ejes, num_ejes))
+ 
+  for(p in dur){
+  for (pasos in 1:p) {
+  for (caminata in 1:cor){
+  for (e in 1:num_ejes){
+    eje<-round(runif(1,1,e))
+     
+      if (runif(1) < 0.5){
+      pos[caminata,eje,e] <- pos[caminata,eje,e] + 1
+      }
+      else{
+       pos[caminata,eje,e]<- pos[caminata,eje,e] - 1
+      }
+   manhattan = sum(abs(pos[caminata,e,])) # suma de los valores absolutos
+    
+   
+ if (p==100){
+  j=1
+}
+  if (p==1000){
+  j=2
+}
+  if (p==10000){
+  j=3
+}
+ mayor[caminata,e,j] = max(mayor[caminata,e,j], manhattan)
+}} 
+}
+}
+for (i in 1:num_ejes){
+ for (j in 1:length(dur)){
+  D=c(mayor[,i,j])
+DISTANCIA=c(DISTANCIA,D)
+}}
+variety=as.factor(c(rep(1:num_ejes, each=cor*length(dur))))
+treatment=rep(c("100","1000","10000"),each=cor)
+data=data.frame(variety, treatment,DISTANCIA)
+library(ggplot2)
+ # grouped boxplot
+ ggplot(data, aes(x=variety, y=DISTANCIA, fill=treatment)) +
+geom_boxplot()
