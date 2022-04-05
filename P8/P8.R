547eabb36df635e433b6f3007b56d15cbce41b97
IsmaelHC <97998567+IsmaelHC@users.noreply.github.com>
2022-04-05 18:54:51 -0500

    Update P8.R

--- P8/P8.R
+++ P8/P8.R
@@ -1,11 +1,36 @@
-#k <- c(10000,50000,150000,75000,400000)
-#n <- c(1000000,2000000,3000000,1000000,4000000)
-k <- c(7500,10000,25000,50000,75000)
-n <- c(1000000)
-replicas=5
-den <- as.data.frame(t(rbind(k,n)))
-names(densidades) <- c("k", "n")
+library(testit) # para pruebas, recuerda instalar antes de usar
+k_i <-c(1500,5000,10000,17500,25000)
+n_i <- c(1000000)
+replicas=10
+den <- as.data.frame(t(rbind(k_i,n_i)))
+names(den) <- c("k_i", "n_i")
 filtracion=numeric()
+for(jj in 1:length(k_i)){
+for(j in 1:replicas){
+k=den$k_i[jj]
+n=den$n_i[jj]
+originales <- rnorm(k)
+cumulos <- originales - min(originales) + 1
+cumulos <- round(n * cumulos / sum(cumulos))
+assert(min(cumulos) > 0)
+diferencia <- n - sum(cumulos)
+if (diferencia > 0) {
+    for (i in 1:diferencia) {
+        p <- sample(1:k, 1)
+        cumulos[p] <- cumulos[p] + 1
+    }
+} else if (diferencia < 0) {
+    for (i in 1:-diferencia) {
+        p <- sample(1:k, 1)
+        if (cumulos[p] > 1) {
+            cumulos[p] <- cumulos[p] - 1
+        }
+    }
+}
+assert(length(cumulos[cumulos == 0]) == 0) # que no haya vacios
+assert(sum(cumulos) == n)
+c <- median(cumulos) # tamanio critico de cumulos
+d <- sd(cumulos) / 4 # factor arbitrario para suavizar la curva
 rotura <- function(x) {
     return (1 / (1 + exp((c - x) / d)))
 }
@@ -13,71 +38,96 @@ union <- function(x) {
     return (exp(-x / c))
 }
 romperse <- function(tam, cuantos) {
-    res <- integer()
-    for (cumulo in 1:cuantos) {
-        if (runif(1) < rotura(tam)) {
-            primera <- sample(1:(tam-1), 1)
-            segunda <- tam - primera
-            res <- c(res, primera, segunda)
-        } else {
-            res <- c(res, tam)
+    romper <- round(rotura(tam) * cuantos) # independientes
+    resultado <- rep(tam, cuantos - romper) # los demas
+    if (romper > 0) {
+        for (cumulo in 1:romper) { # agregar las rotas
+            t <- 1
+            if (tam > 2) { # sample no jala con un solo valor
+                t <- sample(1:(tam-1), 1)
+            }
+            resultado <- c(resultado, t, tam - t)
         }
     }
-    return(res)
+    assert(sum(resultado) == tam * cuantos) # no hubo perdidas
+    return(resultado)
 }
-
-for(jj in 1:length(k)){
-for(j in 1:replicas){
-originales <- rnorm(den$k[jj])
-cumulos <- originales - min(originales) + 1
-cumulos <- round(den$n[jj] * cumulos / sum(cumulos))
-diferencia <- den$n[jj] - sum(cumulos)
-if (diferencia > 0) {
-    for (i in 1:diferencia) {
-        p <- sample(1:den$k[jj], 1)
-        cumulos[p] <- cumulos[p] + 1
-    }
-} else if (diferencia < 0) {
-    for (i in 1:-diferencia) {
-        p <- sample(1:den$k[jj], 1)
-        if (cumulos[p] > 1) {
-            cumulos[p] <- cumulos[p] - 1
-        }
+unirse <- function(tam, cuantos) {
+    unir <- round(union(tam) * cuantos) # independientes
+    if (unir > 0) {
+        division <- c(rep(-tam, unir), rep(tam, cuantos - unir))
+        assert(sum(abs(division)) == tam * cuantos)
+        return(division)
+    } else {
+        return(rep(tam, cuantos))
     }
 }
-c <- median(cumulos) # tamanio critico de cumulos
-d <- sd(cumulos) / 4 # factor arbitrario para suavizar la curva
-
 freq <- as.data.frame(table(cumulos))
 names(freq) <- c("tam", "num")
 freq$tam <- as.numeric(levels(freq$tam))[freq$tam]
 duracion <- 10
 digitos <- floor(log(duracion, 10)) + 1
-xmax <- NULL
 for (paso in 1:duracion) {
-    pedazos <- integer()
-    for (i in 1:dim(freq)[1]) {
+    assert(sum(cumulos) == n)
+    cumulos <- integer()
+    for (i in 1:dim(freq)[1]) { # fase de rotura
         urna <- freq[i,]
-        pedazos <- c(pedazos, romperse(urna$tam, urna$num))
+        if (urna$tam > 1) { # no tiene caso romper si no se puede
+            cumulos <- c(cumulos, romperse(urna$tam, urna$num))
+        } else {
+            cumulos <- c(cumulos, rep(1, urna$num))
+        }
     }
-      freq <- as.data.frame(table(pedazos))
+    assert(sum(cumulos) == n)
+    assert(length(cumulos[cumulos == 0]) == 0) # que no haya vacios
+    freq <- as.data.frame(table(cumulos)) # actualizar urnas
     names(freq) <- c("tam", "num")
     freq$tam <- as.numeric(levels(freq$tam))[freq$tam]
-    filtrados=numeric()
+    assert(sum(freq$num * freq$tam) == n)
+    cumulos <- integer()
+    for (i in 1:dim(freq)[1]) { # fase de union
+        urna <- freq[i,]
+        cumulos <- c(cumulos, unirse(urna$tam, urna$num))
+    }
+    assert(sum(abs(cumulos)) == n)
+    assert(length(cumulos[cumulos == 0]) == 0) # que no haya vacios
+    juntarse <- -cumulos[cumulos < 0]
+    cumulos <- cumulos[cumulos > 0]
+    assert(sum(cumulos) + sum(juntarse) == n)
+    nt <- length(juntarse)
+    if (nt > 0) {
+        if (nt > 1) {
+            juntarse <- sample(juntarse)
+            for (i in 1:floor(nt / 2) ) {
+                cumulos <- c(cumulos, juntarse[2*i-1] + juntarse[2*i])
+            }
+        }
+        if (nt %% 2 == 1) {
+            cumulos <- c(cumulos, juntarse[nt])
+        }
+    }
+    assert(sum(cumulos) == n)
+    freq <- as.data.frame(table(cumulos))
+    names(freq) <- c("tam", "num")
+    freq$tam <- as.numeric(levels(freq$tam))[freq$tam]
+    assert(sum(freq$num * freq$tam) == n)
+ filtrados=numeric()
     for(i in 1:length(freq$tam)){
     if(freq$tam[i]>c){
     filtrados=c(filtrados,(freq$tam[i]*freq$num[i]))
     }
     }
-    filtracion=c(filtracion,(sum(filtrados)/den$n[jj]))
-   }
-}
-}
-densidad=as.character(k/n)
+    filtracion=c(filtracion,(sum(filtrados)/n))
+   
+
+  }
+}}
+densidad=as.character(k_i/n_i)
 treatment=rep(densidad,each=duracion*replicas)
 paso=as.factor(c(rep(1:duracion)))
 data=data.frame((filtracion*100), treatment,paso)
 library(ggplot2)
 ggplot(data, aes(x=paso, y=filtracion*100, fill=treatment)) +
-geom_boxplot()+ggtitle("Filtracion según el valor de k/n en cada paso de la simulacion") +
-  xlab("Paso") + ylab("% de particulas filtradas")+labs(fill = "Tasa k/n"
+geom_boxplot() + xlim(40, 60)+
+  xlab("Paso") + ylab("% de particulas filtradas")+labs(fill = "Tasa k/n")
+
